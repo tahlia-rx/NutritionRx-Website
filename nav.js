795f1d384e20b3e03522c7928ad2e63c2aa59765
tahlia-rx <tahliamay11@gmail.com>
2026-06-07 12:16:23 +1000

    Add files via upload

--- nav.js
+++ nav.js
@@ -3,9 +3,9 @@
 
 (function () {
   const LINKS = [
-    { id: "home",       label: "Home",            href: "https://38ebd151-0711-48f5-b0aa-54f2b20b8835.claudeusercontent.com/v1/design/projects/38ebd151-0711-48f5-b0aa-54f2b20b8835/serve/mobile/index.html?t=e06baa44c9c4c420fc03121a1256f8cfed9c6aff70de0fab4bd49fe251b8d713.e6d27146-04ad-4a14-9966-75253eb72420.7bfd5f30-734a-47d4-814c-40c2be260cab.1780737321.fp&direct=1",       num: "01" },
-    { id: "about",      label: "About Cooper",     href: "https://38ebd151-0711-48f5-b0aa-54f2b20b8835.claudeusercontent.com/v1/design/projects/38ebd151-0711-48f5-b0aa-54f2b20b8835/serve/mobile/about.html?t=ea915ea2edce2a5f0293e23384404a94e0b4460595a948cfbebd67d19dd01de9.e6d27146-04ad-4a14-9966-75253eb72420.7bfd5f30-734a-47d4-814c-40c2be260cab.1780737321.fp&direct=1",       num: "02" },
-    { id: "sneak-peek", label: "App Sneak Peek",  href: "https://38ebd151-0711-48f5-b0aa-54f2b20b8835.claudeusercontent.com/v1/design/projects/38ebd151-0711-48f5-b0aa-54f2b20b8835/serve/mobile/sneak-peek.html?t=74c95805f2782c0aebafd3854acc090a0a80d5191af2e0d86b04108fe4ef19f1.e6d27146-04ad-4a14-9966-75253eb72420.7bfd5f30-734a-47d4-814c-40c2be260cab.1780737322.fp&direct=1",  num: "03" },
+    { id: "home",       label: "Home",           href: "index.html",       num: "01" },
+    { id: "about",      label: "About Cooper",   href: "about.html",       num: "02" },
+    { id: "sneak-peek", label: "App Sneak Peek", href: "sneak-peek.html",  num: "03" },
   ];
 
   function el(html) {
@@ -18,7 +18,7 @@
     // Top bar
     const topbar = el(`
       <header class="topbar" data-comment-anchor="topbar">
-        <a class="brand" href="https://38ebd151-0711-48f5-b0aa-54f2b20b8835.claudeusercontent.com/v1/design/projects/38ebd151-0711-48f5-b0aa-54f2b20b8835/serve/mobile/index.html?t=e06baa44c9c4c420fc03121a1256f8cfed9c6aff70de0fab4bd49fe251b8d713.e6d27146-04ad-4a14-9966-75253eb72420.7bfd5f30-734a-47d4-814c-40c2be260cab.1780737321.fp&direct=1">
+        <a class="brand" href="index.html">
           Nutrition Rx<small>by Dr. Cooper</small>
         </a>
         <button class="hamburger" aria-label="Open menu" aria-expanded="false" aria-controls="primary-menu">
@@ -34,7 +34,7 @@
     const menu = el(`
       <nav class="menu" id="primary-menu" aria-hidden="true" data-comment-anchor="primary-menu">
         <div class="menu__top">
-          <a class="menu__brand" href="https://38ebd151-0711-48f5-b0aa-54f2b20b8835.claudeusercontent.com/v1/design/projects/38ebd151-0711-48f5-b0aa-54f2b20b8835/serve/mobile/index.html?t=e06baa44c9c4c420fc03121a1256f8cfed9c6aff70de0fab4bd49fe251b8d713.e6d27146-04ad-4a14-9966-75253eb72420.7bfd5f30-734a-47d4-814c-40c2be260cab.1780737321.fp&direct=1">Nutrition Rx</a>
+          <a class="menu__brand" href="index.html">Nutrition Rx</a>
           <button class="menu__close" aria-label="Close menu">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
               <line x1="6"  y1="6"  x2="18" y2="18"/>
